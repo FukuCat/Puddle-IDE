f9d42286098e64ec7e94502e3b3db4e80b6ae937
Kenji Fukuoka <kenji_fukuoka@dlsu.edu.ph>
2017-10-09 13:13:36 +0800

    added to toolbar

--- Puddle-Interpreter/src/view/panel/ToolBarPanel.java
+++ Puddle-Interpreter/src/view/panel/ToolBarPanel.java
@@ -1,5 +1,9 @@
 package view.panel;
 
+import view.actions.OpenAction;
+import view.actions.SaveAction;
+import view.actions.TestAction;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -24,6 +28,9 @@ public class ToolBarPanel extends JPanel {
         btn_array.add(new JButton("Open"));
         btn_array.add(new JButton("Save"));
         btn_array.add(new JButton("Test"));
+        btn_array.get(0).addActionListener(new OpenAction());
+        btn_array.get(1).addActionListener(new SaveAction());
+        btn_array.get(2).addActionListener(new TestAction());
         for(JButton btn : btn_array){
             btn.setMaximumSize(new Dimension(100, 30));
         }
