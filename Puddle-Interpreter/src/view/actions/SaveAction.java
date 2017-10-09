67204ec88124f38dc58823e2fc1c28662394e0ae
Kenji Fukuoka <kenji_fukuoka@dlsu.edu.ph>
2017-10-09 13:13:11 +0800

    added actions

---
+++ Puddle-Interpreter/src/view/actions/SaveAction.java
@@ -0,0 +1,11 @@
+package view.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SaveAction implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ Puddle-Interpreter/src/view/actions/TestAction.java
@@ -0,0 +1,11 @@
+package view.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TestAction implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
