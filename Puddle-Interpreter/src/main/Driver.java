9e54070ae79e926cc50de04cfd9e6846d14e809f
Kenji Fukuoka <kenji_fukuoka@dlsu.edu.ph>
2017-10-22 16:11:44 +0800

    kulang nlng updated g4 file and ide features

--- Puddle-Interpreter/src/main/Driver.java
+++ Puddle-Interpreter/src/main/Driver.java
@@ -18,30 +18,39 @@ import java.io.IOException;
 // parser added reserved keywords rule to "type" rule
 public class Driver {
     public static void main(String[] args){
-        //new MainWindow("Puddle IDE",1280,720);
+        new MainWindow("Puddle IDE",1280,720);
+        /*
         String text =
-                "fun myFunction(x: Int): Int {\n" +
-                        "\tval add: Int =  x + 50 + 1; //syntax error #1\n" +
+                "//Convert this into your language rule syntax.\n" +
+                        "\n" +
+                        "fun testOne() {\n" +
+                        "\tval x: Int =  0;\n" +
+                        "\tscan(\"Hello. I need your number: \" ,x);\n" +
                         "\t\n" +
-                        "\treturn add; //syntax error #2\n" +
+                        "\tfor(i in 1..10) { //syntax error #1\n" +
+                        "\t\tprint(\"Petmalu \" +x); //syntax error #2\n" +
+                        "\t\t\n" +
+                        "\t}//syntax error #3 (missing curly brace)\n" +
                         "}\n" +
                         "\n" +
-                        "fun myFunctionFloat(x: Float): Float {\n" +
-                        "\tval fTest: Float = ((x * 50) + (x * 15)); //syntax error #3\n" +
-                        "\t\n" +
-                        "\treturn fTest * 50 (); //syntax error #4\n" +
+                        "fun testTwo(x: Float) {\n" +
+                        "\tif(x = 4)//syntax error #4 \n" +
+                        "\t{\n" +
+                        "\t\tprint(x is a very huge number); //syntax error #5\n" +
+                        "\t} \n" +
                         "}\n" +
-                        "\n" +
                         "fun main(args : Array<String>) {\n" +
-                        "\tthis.myFunction(50);\n" +
-                        "\tthis.myFunctionFloat(50.0f);\n" +
-                        "}";
+                        "\tfloat value = (5 * 1) + (5 * 5 / 1 + 3 + (4 + (5 * 3)))/2.0f * 8.0f;\n" +
+                        "\t\n" +
+                        "\tthis.testOne()(x); //syntax error #6\n" +
+                        "\tthis.testTwo(123 ++value); //syntax error #7\n" +
+                        "}\n";
         KotlinParserFacade parser = new KotlinParserFacade();
         try {
             ParseTreeWalker.DEFAULT.walk(new AntlrKotlinListener(), parser.parseString(text));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        */
     }
 }
--- Puddle-Interpreter/src/main/error/ParserErrorListener.java
+++ Puddle-Interpreter/src/main/error/ParserErrorListener.java
@@ -45,7 +45,7 @@ public class ParserErrorListener extends BaseErrorListener {
                                         "[" + msg + "]\n");
                 }
             }
-            hasError = true;
+            //hasError = true;
         }
     }
 
